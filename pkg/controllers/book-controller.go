73937becb87bdd867514f52100aa76d24fcbf05f
Moazam Ali <contactmoazam@gmail.com>
2025-04-28 13:59:18 +0500

    structure

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/MoazamAli/go-bookstore/pkg/models"
+	"github.com/MoazamAli/go-bookstore/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBook := models.GetAllBooks()
+	res, _ := json.Marshal(newBook)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookid := vars["bookId"]
+	ID, err := strconv.ParseInt(bookid, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
